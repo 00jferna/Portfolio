1dce80583dd3f3b07e8dcf89931bca85c7774557
Jesse Fernandez <jfernandez@coderunner.studio>
2023-11-09 00:45:17 -0600

    Updating base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
-    site:'https://00jferna.github.io'
-    base:'/Portfolio'
+export default defineConfig({
+  site: "https://00jferna.github.io",
+  base: "/Portfolio",
+});
